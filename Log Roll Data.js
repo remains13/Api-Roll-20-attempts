256ff589678e0cd48a52ee6e6596b25bc7d2c080
remains13 <remains13@gmail.com>
2015-10-20 09:07:09 +0800

    Update Log Roll Data.js

--- Log Roll Data.js
+++ Log Roll Data.js
@@ -1,15 +1,13 @@
 					//run this then use macro, copy log :D
-on("chat:message", function(msg) {
+on("chat:message", function(msg){
 	
 	var rollinfo = JSON.parse(msg.content);
 
 
 												//gonna do stuff here
 						
-	if (msg.type == "rollresult") {
-	log(rollinfo);
-	}
-
+	
+	log(rollinfo.content);
 	
 
 	});
