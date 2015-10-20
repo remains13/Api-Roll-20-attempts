2c64caf078367b459f9a7e460905a01d89f929db
remains13 <remains13@gmail.com>
2015-10-20 13:39:10 +0800

    Update Crit Failure.js

--- Crit Failure.js
+++ Crit Failure.js
@@ -1,27 +1,55 @@
+// a roll following this format is assumed: /r xdx "name of table rolling on"
+
 on("chat:message", function(msg) {
-	
-	var rollinfo = JSON.parse(msg.content);
+
 	var tablename;
-														//what table are we rolling?
-	
-	if (msg.content.indexOf("Critical Fail with Melee Weapon") !== -1) {
-		tablename = "CritFail-Melee-with-Weapon";
-	
-	}
 
-	else if (msg.content.indexOf("Critical Success or Failure") !== -1) {
-		tablename = "CritFail-Unarmed-or-Natural-Weapons";
-	
-	}
-														// other tables will go here
-	
-	
-	
-	if (msg.type == "rollresult" && rollinfo.content.total <= 50) {
+	if (msg.type == "rollresult") {
 
-	sendChat(msg.who, "[[1t[" + tablename + "]]]");		// gets sent to chat (reformat to suit)
+		var rollinfo = JSON.parse(msg.content);
+
+		
+		if (rollinfo.rolls[0].results[0].v <= 50){
+		
+		var isCrit = function() {								//determining table name and if roll is crit check
+			if (rollinfo.rolls[1].text.indexOf("Critical Fail with Melee Weapon") !== -1) {
+			
+				tablename = "CritFail-Melee-with-Weapon";
+				
+				return true;
+				
+			}
+		
+			else if (rollinfo.rolls[1].text.indexOf("Critical Success or Failure") !== -1) {
+			
+				tablename = "CritFail-Unarmed-or-Natural-Weapons";
+				
+				return true;
+				
+			}
+			
+			else {
+			
+				return false;
+				
+			}
+																		//other tables can go here 
+		};
+		
+		if (isCrit === true) {			
+			sendChat(msg.who, "[[1t[" + tablename + "]]]");		
+      
+      // gets sent to chat (reformat to suit)
+      
+		}
+		}
+		
+		
+		
+		}
 	
-	}							
-	
+
+    
 	
 	});
+	
